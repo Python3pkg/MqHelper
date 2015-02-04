0ff93e24dd7da759182fe981b315d0c091219591
Paul Klingelhuber <paul.klingelhuber@catalysts.cc>
2015-02-04 21:39:55 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,18 @@
+# MqHelper
+
+The extremely simple Wrapper around python mosquitto
+
+To install run
+
+```
+python setup.py install
+```
+
+Supports:
+
+* automatic reconnect
+* buffering of one pending message (automatically sent after reconnect)
+* management of different callback methods for different topics
+
+< 100 lines of python!
+
---
+++ package/MqHelper.py
@@ -0,0 +1,84 @@
+
+class MqHelper:
+
+	def __init__(self, name=None, host=None):
+		self.client = None
+		self.pending = None
+		self.host = host or '127.0.0.1'
+		self.clientName = name or 'testClient'
+		self.subscriptions = {}
+
+	def loop(self):
+		if self.client == None:
+			self.reconnect()
+		ret = self.client.loop()
+		if ret != 0:
+			print("mosquitto client error")
+			try:
+				self.client.disconnect()
+			except:
+				pass
+			self.client = None
+
+	def send(self, topic, msg):
+		if self.client:
+			ret = -1
+			try:
+				ret = self.client.publish(topic, msg, 1)
+			except:
+				print("error sending msg")
+			if ret == 0 or len(ret) > 1 and ret[0] == 0:
+				# sending success
+				self.pending = None
+			else:
+				print("returned %s"%str(ret))
+				print("saving pending msg %s %s"%(topic, msg))
+				self.pending = (topic, msg)
+				self.reconnect()				
+		else:
+			print("client not available, saving as pending")
+			self.pending = (topic, msg)
+			self.reconnect()
+
+	def onConnect(self):
+		def callback(mosq, obj, rc):
+			if rc != 0:
+				self.client = None
+				print("connecting failed")
+			else:
+				self.client.on_message = self.createMsgCallback()
+				self.__subscribeAll()
+				print("connected")
+				if self.pending:
+					print("sending pending message %s"%self.pending[1])
+					self.send(self.pending[0], self.pending[1])
+		return callback
+
+	def __subscribeAll(self):
+		for topic in self.subscriptions.keys():
+			self.client.subscribe(topic, 0)
+
+	def subscribe(self, topic, callback):
+		self.subscriptions[topic] = callback
+		if self.client != None:
+			self.client.subscribe(topic, 0)
+
+	def createMsgCallback(self):
+		def callback(mosq, obj, msg):
+			print("received msg on %s"%msg.topic)
+			if self.subscriptions.has_key(msg.topic):
+				self.subscriptions[msg.topic](msg.topic, msg.payload)
+		return callback
+
+	def reconnect(self):
+		import mosquitto
+		print("reconnecting...")
+		try:
+			self.client = mosquitto.Mosquitto(self.clientName)
+			ret = self.client.connect(self.host)
+			if ret == 0:
+				self.client.on_connect = self.onConnect()
+			else:
+				self.client = None
+		except:
+			self.client = None
